fbd569fdbbcb476fa38cfccf7524a77e8d29a760
katros01 <kairos.mutabaruka@amalitechtraining.org>
2024-10-06 07:58:44 +0200

    refactor terraform file

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+variable "aws_access_key_id" {
+  description = "The AWS Access Key ID"
+  type        = string
+}
+
+variable "aws_secret_access_key" {
+  description = "The AWS Secret Access Key"
+  type        = string
+}
+
+
 # AWS Provider
 provider "aws" {
   region = "us-east-1"
