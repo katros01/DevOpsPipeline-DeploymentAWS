d144a1c285421a154883e59895d2bd8d2e23a638
katros01 <kairos.mutabaruka@amalitechtraining.org>
2024-10-06 08:21:39 +0200

    refactor terraform file

--- main.tf
+++ main.tf
@@ -1,4 +1,12 @@
+variable "aws_access_key_id" {
+  description = "The AWS Access Key ID"
+  type        = string
+}
 
+variable "aws_secret_access_key" {
+  description = "The AWS Secret Access Key"
+  type        = string
+}
 
 
 # AWS Provider
